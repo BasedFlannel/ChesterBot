d33de20622e462a767055c97247fa03f387a69a7
Coco <heyanyaray@gmail.com>
2025-07-05 21:42:45 -0700

    base POST implementation

--- restUtil.go
+++ restUtil.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -19,4 +22,52 @@ func restGet(url string) string {
 	errorCheck(err)
 
 	return string(data)
+}
+
+func restPut() {
+	// Replace with the API ID provided by crudcrud.com
+	apiID := "XXXXXX"
+
+	// HTTP endpoint with path of resource (vehicles)
+	createResourceURL := fmt.Sprintf("https://crudcrud.com/api/%s/vehicles",
+		apiID)
+
+	// JSON payload containing resource information
+	requestBody, _ := json.Marshal(map[string]string{
+		"color":     "White",
+		"license":   "ABC123",
+		"numWheels": "4",
+		"type":      "Car",
+	})
+
+	// Converting request body into bytes buffer
+	requestBodyBytes := bytes.NewBuffer(requestBody)
+
+	// Creating a POST request on createResourceURL
+	request, err := http.NewRequest("POST", createResourceURL, requestBodyBytes)
+	if err != nil {
+		panic(err)
+	}
+
+	// Adding application/json as payload type
+	request.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	// Performing POST request
+	response, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	// Reading the response to the request
+	responseBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error received in response:", err)
+	}
+
+	responseBodyString := string(responseBody)
+	fmt.Println(responseBodyString)
 }
